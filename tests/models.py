53ed7fcb9b9f04fcdb75a3b21b3184a75b564539
stasp <stasp@ronis.mail>
2020-08-14 18:00:13 +0300

    test cases list on test update

--- tests/models.py
+++ tests/models.py
@@ -13,13 +13,14 @@ class Test(models.Model):
 class TestCase(models.Model):
     test = models.ForeignKey(Test, on_delete=models.CASCADE, null=True, blank=True)
     content = models.CharField(max_length=255, null=True)
+    score = models.DecimalField(max_digits=2, decimal_places=1, null=True)
 
 
 class Answer(models.Model):
     users_answers = models.ManyToManyField(User, related_name="users_answers")
     test_case = models.ForeignKey(TestCase, on_delete=models.CASCADE)
     content = models.CharField(max_length=255, null=True, blank=True)
-
+    is_correct = models.BooleanField(default=False)
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
